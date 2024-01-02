54a84322dee25890de1af6eb1d1ba18d1e6865d3
kkj6235 <kkj6235@ajou.ac.kr>
2024-01-02 12:54:04 +0900

    initial commit

---
+++ src/main/java/Honzapda/Honzapda_server/HonzapdaServerApplication.java
@@ -0,0 +1,13 @@
+package Honzapda.Honzapda_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonzapdaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonzapdaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Honzapda/Honzapda_server/HonzapdaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Honzapda.Honzapda_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonzapdaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
