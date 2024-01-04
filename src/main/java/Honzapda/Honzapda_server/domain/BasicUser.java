d3a2abc30a82d3530948a317eacc81581d07a38a
KiSeungMin <ksm011212@gmail.com>
2024-01-04 12:32:13 +0900

    Feat: Create BaseEntity, User, BasicUser, AppleUser Entity

---
+++ src/main/java/Honzapda/Honzapda_server/domain/BasicUser.java
@@ -0,0 +1,22 @@
+package Honzapda.Honzapda_server.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BasicUser extends User{
+
+    @Column(nullable = false, length = 50)
+    private String account;
+
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+}
---
+++ src/main/java/Honzapda/Honzapda_server/domain/User.java
@@ -0,0 +1,31 @@
+package Honzapda.Honzapda_server.domain;
+
+import Honzapda.Honzapda_server.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    private String address;
+
+    @Column(length = 50)
+    private String address_spec;
+
+    @Column(nullable = false, length = 25)
+    private String name;
+
+    private LocalDateTime inactiveDate;
+}
