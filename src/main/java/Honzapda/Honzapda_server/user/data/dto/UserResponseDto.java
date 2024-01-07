dd583229aa5b79400a04c43d8d0c806353dcc8e1
loading1031 <tjdans1031@gmail.com>
2024-01-07 19:42:01 +0900

    📄Added user request/response DTO files

---
+++ src/main/java/Honzapda/Honzapda_server/user/data/dto/UserResponseDto.java
@@ -0,0 +1,8 @@
+package Honzapda.Honzapda_server.user.data.dto;
+
+/*
+특수) api + dto -> 회원가입 dto : registerDto
+일반) method + 대상 + dto -> 리뷰 리스트 조회 dto : GetReviewListDto
+*/
+public class UserResponseDto {
+}
