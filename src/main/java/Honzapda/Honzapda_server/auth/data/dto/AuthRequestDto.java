58c55e1978c15ef5bbb71d8bb94ea9e15943641a
loading1031 <tjdans1031@gmail.com>
2024-01-08 19:24:22 +0900

    :sparkles: Feat: Create EmailRequestDTO and SignUpRequestDTO

---
+++ src/main/java/Honzapda/Honzapda_server/auth/data/dto/AuthRequestDto.java
@@ -0,0 +1,27 @@
+package Honzapda.Honzapda_server.auth.data.dto;
+
+import Honzapda.Honzapda_server.auth.validation.annotation.CheckId;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+/*
+특수) api -> 회원가입 : register
+일반) method + 대상 + dto -> 리뷰 리스트 조회 : GetReviewList
+ */
+public class AuthRequestDto {
+    @Getter
+    public static class GetEmail{
+        @Size(max=50)
+        @CheckId
+        @NotBlank
+        @Email
+        String email;
+    }
+    @Getter
+    public static class Register{
+        String email;
+        String password;
+    }
+}
