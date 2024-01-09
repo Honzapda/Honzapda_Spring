5b7ddff02555917aeed3a31843c0eb25144ef42e
loading1031 <tjdans1031@gmail.com>
2024-01-09 15:10:20 +0900

    :sparkles: Feat: create methods to login generate random name

--- src/main/java/Honzapda/Honzapda_server/auth/service/AuthService.java
+++ src/main/java/Honzapda/Honzapda_server/auth/service/AuthService.java
@@ -1,9 +1,12 @@
 package Honzapda.Honzapda_server.auth.service;
 
 import Honzapda.Honzapda_server.auth.data.dto.AuthRequestDto;
+import Honzapda.Honzapda_server.auth.data.dto.AuthResponseDto;
+import Honzapda.Honzapda_server.user.data.entity.User;
 
 public interface AuthService {
     boolean isEMail(String email);
     void registerUser(AuthRequestDto.Register request);
     String genName();
+    AuthResponseDto.Login loginUser(AuthRequestDto.Login request);
 }
