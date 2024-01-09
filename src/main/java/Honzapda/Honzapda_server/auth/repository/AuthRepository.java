acc719120d02e399b2f87474668b27e624fdea5e
loading1031 <tjdans1031@gmail.com>
2024-01-09 15:07:58 +0900

    :sparkles: Feat: find user by name and email

--- src/main/java/Honzapda/Honzapda_server/auth/repository/AuthRepository.java
+++ src/main/java/Honzapda/Honzapda_server/auth/repository/AuthRepository.java
@@ -3,7 +3,10 @@ package Honzapda.Honzapda_server.auth.repository;
 import Honzapda.Honzapda_server.user.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByName(String name);
+    Optional<User> findByEmail(String email);
 }
