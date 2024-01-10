75da765d1e8f9f610d152b2f0e58bfbd3a61c360
kkj6235 <kkj6235@ajou.ac.kr>
2024-01-11 01:07:12 +0900

    feat: Enable Feign clients, configuration properties, and JPA auditing in HonzapdaServerApplication

--- src/main/java/Honzapda/Honzapda_server/HonzapdaServerApplication.java
+++ src/main/java/Honzapda/Honzapda_server/HonzapdaServerApplication.java
@@ -2,8 +2,15 @@ package Honzapda.Honzapda_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableConfigurationProperties
+@EnableJpaAuditing
 public class HonzapdaServerApplication {
 
 	public static void main(String[] args) {
