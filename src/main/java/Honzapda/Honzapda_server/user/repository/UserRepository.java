59b39a069649aa8d5900eb7c7efbbc016ab3889f
kkj6235 <kkj6235@ajou.ac.kr>
2024-01-11 01:05:13 +0900

    feat: Add findByEmail method to UserRepository

--- src/main/java/Honzapda/Honzapda_server/user/repository/UserRepository.java
+++ src/main/java/Honzapda/Honzapda_server/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package Honzapda.Honzapda_server.user.repository;
 import Honzapda.Honzapda_server.user.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
