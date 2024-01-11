115bb279696e633feb2f06652c1855641bd62a54
loading1031 <tjdans1031@gmail.com>
2024-01-11 21:49:52 +0900

    Feat: add Login and Join Error status

--- src/main/java/Honzapda/Honzapda_server/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/Honzapda/Honzapda_server/apiPayload/code/status/ErrorStatus.java
@@ -13,11 +13,12 @@ public enum ErrorStatus implements BaseErrorCode {
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"COMMON500","서버 에러, 관리자에게 문의 바랍니다."),
     _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON400","잘못된 요청입니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","인증이 필요합니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
     // 회원가입 응답
 
+    EMAIL_EXIST(HttpStatus.BAD_REQUEST,"JOIN4001","이미 존재하는 계정입니다."),
     // 로그인 응답
-
+    LOGIN_NOT_MATCH(HttpStatus.BAD_REQUEST,"LOGIN4002","ID혹은PW가 다릅니다."),
     // 유저 응답
 
 
