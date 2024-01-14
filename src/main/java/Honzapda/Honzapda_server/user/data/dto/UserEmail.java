57fb44a5bde31c3a999eecb9d9bb7ed2597fef63
loading1031 <tjdans1031@gmail.com>
2024-01-14 17:30:59 +0900

    :recycle: Refactor: Integrating User Domain with Auth Domain(repository, dto, service)

---
+++ src/main/java/Honzapda/Honzapda_server/user/data/dto/UserEmail.java
@@ -0,0 +1,12 @@
+package Honzapda.Honzapda_server.user.data.dto;
+
+import Honzapda.Honzapda_server.user.validation.annotation.UniqueEmail;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+
+@Getter
+public class UserEmail {
+    @Email
+    @UniqueEmail
+    private String email;
+}
