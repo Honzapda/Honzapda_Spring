e115a3d198b3457027348ba7a22b8971ff5a3ff1
loading1031 <tjdans1031@gmail.com>
2024-01-14 20:48:56 +0900

    :recycle: Refactor: Rename Dto Class name

--- src/main/java/Honzapda/Honzapda_server/user/data/dto/FindEmailDto.java
+++ src/main/java/Honzapda/Honzapda_server/user/data/dto/FindEmailDto.java
@@ -3,7 +3,7 @@ package Honzapda.Honzapda_server.user.data.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 @Getter
-public class UserNickNameDto {
+public class FindEmailDto {
     @NotBlank
     private String name;
 }
