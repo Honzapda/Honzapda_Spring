9165be58b9daab1f76f542df42ddfa63bebfcc78
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2024-01-17 20:28:16 +0900

    fix : Delete GCSRequestDTO

--- src/main/java/Honzapda/Honzapda_server/file/service/FileService.java
+++ src/main/java/Honzapda/Honzapda_server/file/service/FileService.java
@@ -1,6 +1,5 @@
 package Honzapda.Honzapda_server.file.service;
 
-import Honzapda.Honzapda_server.file.data.dto.GCSRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
