b6b204988b0c25f4058b9a94b25c864737ce0971
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2024-01-19 20:45:57 +0900

    refactor : Make partial modifications for applying private key in Apple social login.

--- src/main/java/Honzapda/Honzapda_server/auth/apple/AppleProperties.java
+++ src/main/java/Honzapda/Honzapda_server/auth/apple/AppleProperties.java
@@ -5,6 +5,10 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @Component
 @ConfigurationProperties(prefix = "social-login.provider.apple")
 @Getter
@@ -16,5 +20,14 @@ public class AppleProperties {
     private String keyId;
     private String teamId;
     private String audience;
-    private String privateKey;
+    private String privateKeyPath;
+    public String getPrivateKey() {
+        try {
+            // privateKeyPath 변수를 사용하여 p8 키 파일의 위치를 지정
+            byte[] privateKeyBytes = Files.readAllBytes(Paths.get(privateKeyPath));
+            return new String(privateKeyBytes);
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading private key file", e);
+        }
+    }
 }
