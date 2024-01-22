920d888860fbb8421b47a80321f7e133d2fa6271
loading1031 <tjdans1031@gmail.com>
2024-01-22 21:19:31 +0900

    :recycle: Refactor: seperate ReviewImageDto from ReviewDto

---
+++ src/main/java/Honzapda/Honzapda_server/review/data/dto/ReviewImageResponseDto.java
@@ -0,0 +1,31 @@
+package Honzapda.Honzapda_server.review.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class ReviewImageResponseDto {
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ImageDto{
+        private Long imageId;
+        private String url;
+    }
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ImageListDto{
+        private List<ImageDto> imageDtoList;
+        private Integer getNumberOfElements;
+        private Boolean hasNext;
+        private Boolean hasPrevious;
+    }
+
+
+}
