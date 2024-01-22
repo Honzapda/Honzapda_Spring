f2e3ffce4e44feef42aa5a166012c1bf5d41f6aa
loading1031 <tjdans1031@gmail.com>
2024-01-22 21:17:30 +0900

    :sparkles: Feat: create method that gets review image method

--- src/main/java/Honzapda/Honzapda_server/review/repository/mysql/ReviewImageRepository.java
+++ src/main/java/Honzapda/Honzapda_server/review/repository/mysql/ReviewImageRepository.java
@@ -2,6 +2,9 @@ package Honzapda.Honzapda_server.review.repository.mysql;
 
 import Honzapda.Honzapda_server.review.data.entity.Review;
 import Honzapda.Honzapda_server.review.data.entity.ReviewImage;
+import Honzapda.Honzapda_server.shop.data.entity.Shop;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +12,6 @@ import java.util.Optional;
 
 public interface ReviewImageRepository extends JpaRepository<ReviewImage,Long> {
     Optional<List<ReviewImage>> findAllByReview(Review review);
+    Slice<ReviewImage> findAllByShopOrderByCreatedAtDesc(Shop shop, Pageable pageable);
 
 }
