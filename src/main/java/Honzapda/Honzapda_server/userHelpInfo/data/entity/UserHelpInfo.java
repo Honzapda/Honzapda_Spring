de36e69b1f5b54fb6a8798d924757fd5d1a10b35
CJY <c0512jy@naver.com>
2024-01-26 23:40:52 +0900

    ✨feat: map home v2

--- src/main/java/Honzapda/Honzapda_server/userHelpInfo/data/entity/UserHelpInfo.java
+++ src/main/java/Honzapda/Honzapda_server/userHelpInfo/data/entity/UserHelpInfo.java
@@ -23,15 +23,19 @@ public class UserHelpInfo extends BaseEntity {
     private LocalDateTime visitDate;
 
     // 혼잡도: 10%, 20%, 30%, 40%, 50%, 60%, 70%, 80%, 90%, 100%
+    @Enumerated(EnumType.STRING)
     private Congestion congestion;
 
     // 앉았던 책상의 넓이: 넓었어요, 적당했어요, 좁았어요, 기억나지 않아요
+    @Enumerated(EnumType.STRING)
     private DeskSize deskSize;
 
     // 콘센트 개수: 넉넉했어요, 적당했어요, 부족했어요, 기억나지 않아요
+    @Enumerated(EnumType.STRING)
     private OutletCount outletCount;
 
     // 카페 조명은 어떻게 느껴졌나요?: 밝았어요, 적당했어요, 어두웠어요, 기억나지 않아요
+    @Enumerated(EnumType.STRING)
     private Light light;
 
     // 콘센트는 주로 어디에 있었나요?: 직접 입력, 기억나지 않아요
