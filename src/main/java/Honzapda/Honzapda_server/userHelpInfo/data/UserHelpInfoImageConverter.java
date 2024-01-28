c9a6f069ea6ca44a6b974c58820c5cd1b9ca4257
loading1031 <tjdans1031@gmail.com>
2024-01-29 02:32:43 +0900

    :recycle: Refactor: create toImageEntity and rename toImageEntity

--- src/main/java/Honzapda/Honzapda_server/userHelpInfo/data/UserHelpInfoImageConverter.java
+++ src/main/java/Honzapda/Honzapda_server/userHelpInfo/data/UserHelpInfoImageConverter.java
@@ -8,9 +8,15 @@ import Honzapda.Honzapda_server.userHelpInfo.data.entity.UserHelpInfoImage;
 import java.util.List;
 
 public class UserHelpInfoImageConverter {
-    public static List<UserHelpInfoImage> toImageEntity(UserHelpInfoRequestDto.CreateDto createDto, UserHelpInfo savedUserHelpInfo) {
+    public static UserHelpInfoImage toImageEntity(String imageUrl,UserHelpInfo userHelpInfo){
+        return UserHelpInfoImage.builder()
+                .url(imageUrl)
+                .userHelpInfo(userHelpInfo)
+                .build();
+    }
+    public static List<UserHelpInfoImage> toImages(UserHelpInfoRequestDto.CreateDto createDto, UserHelpInfo savedUserHelpInfo) {
         return createDto.getImageUrls().stream()
-                .map(imageUrl -> UserHelpInfoImage.createImage(imageUrl, savedUserHelpInfo))
+                .map(imageUrl -> UserHelpInfoImageConverter.toImageEntity(imageUrl, savedUserHelpInfo))
                 .toList();
     }
     public static UserHelpInfoImageResponseDto.ImageDto toImageDto(UserHelpInfoImage entity){
