6b2e22862617a73053e88200b5832daea0c726cc
loading1031 <tjdans1031@gmail.com>
2024-01-29 02:34:02 +0900

    :sparkles: Feat:

--- src/main/java/Honzapda/Honzapda_server/userHelpInfo/repository/UserHelpInfoImageRepository.java
+++ src/main/java/Honzapda/Honzapda_server/userHelpInfo/repository/UserHelpInfoImageRepository.java
@@ -1,7 +1,12 @@
 package Honzapda.Honzapda_server.userHelpInfo.repository;
 
+import Honzapda.Honzapda_server.userHelpInfo.data.entity.UserHelpInfo;
 import Honzapda.Honzapda_server.userHelpInfo.data.entity.UserHelpInfoImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserHelpInfoImageRepository extends JpaRepository<UserHelpInfoImage, Long> {
+    //TODO:좋아요 추가해서, 좋아요 수로 변경해야함
+    List<UserHelpInfoImage> findAllByUserHelpInfo(UserHelpInfo userHelpInfo);
 }
