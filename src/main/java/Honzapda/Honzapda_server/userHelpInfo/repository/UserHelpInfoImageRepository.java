345561704cea753dc0a38bba438c1b90a9be08de
loading1031 <tjdans1031@gmail.com>
2024-01-29 13:45:14 +0900

    :recycle: Refactor: change findAllByUserHelpInfo's return type

--- src/main/java/Honzapda/Honzapda_server/userHelpInfo/repository/UserHelpInfoImageRepository.java
+++ src/main/java/Honzapda/Honzapda_server/userHelpInfo/repository/UserHelpInfoImageRepository.java
@@ -5,8 +5,9 @@ import Honzapda.Honzapda_server.userHelpInfo.data.entity.UserHelpInfoImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserHelpInfoImageRepository extends JpaRepository<UserHelpInfoImage, Long> {
     //TODO:좋아요 추가해서, 좋아요 수로 변경해야함
-    List<UserHelpInfoImage> findAllByUserHelpInfo(UserHelpInfo userHelpInfo);
+    Optional<List<UserHelpInfoImage>> findAllByUserHelpInfo(UserHelpInfo userHelpInfo);
 }
