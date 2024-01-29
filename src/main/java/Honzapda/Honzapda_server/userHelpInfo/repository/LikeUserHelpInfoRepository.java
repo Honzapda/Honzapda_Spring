98414c95d7485cc495fb78f000885145032b9e99
loading1031 <tjdans1031@gmail.com>
2024-01-29 20:28:31 +0900

    :sparkles: Feat: create LikeUserHelpInfoRepository

---
+++ src/main/java/Honzapda/Honzapda_server/userHelpInfo/repository/LikeUserHelpInfoRepository.java
@@ -0,0 +1,13 @@
+package Honzapda.Honzapda_server.userHelpInfo.repository;
+
+import Honzapda.Honzapda_server.user.data.entity.User;
+import Honzapda.Honzapda_server.userHelpInfo.data.entity.UserHelpInfo;
+import Honzapda.Honzapda_server.userHelpInfo.data.entity.mapping.LikeUserHelpInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LikeUserHelpInfoRepository extends JpaRepository<LikeUserHelpInfo,Long> {
+    Boolean existsByUserAndUserHelpInfo(User user, UserHelpInfo userHelpInfo);
+    Optional<LikeUserHelpInfo> findByUserAndUserHelpInfo(User user, UserHelpInfo userHelpInfo);
+}
