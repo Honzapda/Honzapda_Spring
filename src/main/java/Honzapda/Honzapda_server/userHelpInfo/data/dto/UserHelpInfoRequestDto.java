177cb9e597cb007f4a8acbe2cd130a5ffcc0e952
loading1031 <tjdans1031@gmail.com>
2024-01-29 22:48:58 +0900

    :safety_vest: Add: NotNull annotation

--- src/main/java/Honzapda/Honzapda_server/userHelpInfo/data/dto/UserHelpInfoRequestDto.java
+++ src/main/java/Honzapda/Honzapda_server/userHelpInfo/data/dto/UserHelpInfoRequestDto.java
@@ -2,6 +2,7 @@ package Honzapda.Honzapda_server.userHelpInfo.data.dto;
 
 import Honzapda.Honzapda_server.userHelpInfo.data.entity.*;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.util.List;
@@ -15,18 +16,22 @@ public class UserHelpInfoRequestDto {
 
         // 혼잡도: 10%, 20%, 30%, 40%, 50%, 60%, 70%, 80%, 90%, 100%
         @Schema(description = "available inputs : 10, 20,... , 90")
+        @NotNull(message = "형식에 맞지 않는 값입니다.")
         private Congestion congestion;
 
         // 앉았던 책상의 넓이: 넓었어요, 적당했어요, 좁았어요, 기억나지 않아요
         @Schema(description = "available inputs : small, medium, large, none")
+        @NotNull(message = "형식에 맞지 않는 값입니다.")
         private DeskSize deskSize;
 
         // 콘센트 개수: 넉넉했어요, 적당했어요, 부족했어요, 기억나지 않아요
         @Schema(description = "available inputs : enough, adequate, lack, none")
+        @NotNull(message = "형식에 맞지 않는 값입니다.")
         private OutletCount outletCount;
 
         // 카페 조명은 어떻게 느껴졌나요?: 밝았어요, 적당했어요, 어두웠어요, 기억나지 않아요
         @Schema(description = "available inputs : bright, adequate, dark, none")
+        @NotNull(message = "형식에 맞지 않는 값입니다.")
         private Light light;
 
         // 콘센트는 주로 어디에 있었나요?: 직접 입력, 기억나지 않아요
