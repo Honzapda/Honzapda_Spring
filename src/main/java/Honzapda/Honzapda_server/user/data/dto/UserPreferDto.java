cf9956cb52466dc0a5a9091b1537e90de84c83c5
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2024-02-02 00:08:51 +0900

    refactor : apply @NoArgsConstructor and @AllArgsConstructor to PreferDto

--- src/main/java/Honzapda/Honzapda_server/user/data/dto/UserPreferDto.java
+++ src/main/java/Honzapda/Honzapda_server/user/data/dto/UserPreferDto.java
@@ -1,11 +1,15 @@
 package Honzapda.Honzapda_server.user.data.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UserPreferDto {
     List<String> preferNameList;
