73198ed8b7574f6b231b04bd6358f5088562bd85
loading1031 <tjdans1031@gmail.com>
2024-02-06 19:36:46 +0900

    :heavy_minus_sign: Remove: unused import statements

--- src/main/java/Honzapda/Honzapda_server/userHelpInfo/data/dto/UserHelpInfoRequestDto.java
+++ src/main/java/Honzapda/Honzapda_server/userHelpInfo/data/dto/UserHelpInfoRequestDto.java
@@ -5,8 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
-import java.util.List;
-
 public class UserHelpInfoRequestDto {
 
     @Getter
