082bd7caa35b43ddb335a918935c6d1f0d88ea33
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2024-03-25 20:09:35 +0900

    feat: Add OpenAPI default server configuration

--- src/main/java/Honzapda/Honzapda_server/HonzapdaServerApplication.java
+++ src/main/java/Honzapda/Honzapda_server/HonzapdaServerApplication.java
@@ -1,5 +1,7 @@
 package Honzapda.Honzapda_server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,6 +10,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableConfigurationProperties
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server url")
+		}
+)
 public class HonzapdaServerApplication {
 
 	public static void main(String[] args) {
